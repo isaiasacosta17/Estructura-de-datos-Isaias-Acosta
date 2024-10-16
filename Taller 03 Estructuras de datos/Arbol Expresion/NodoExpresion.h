3104655f30d058eccdd670ee8704711ca5a035c6
isaiasacosta17 <isaiasacosta173@gmail.com>
2024-10-16 11:48:44 -0500

    Update NodoExpresion.h

--- Taller 03 Estructuras de datos/Arbol Expresion/NodoExpresion.h
+++ Taller 03 Estructuras de datos/Arbol Expresion/NodoExpresion.h
@@ -1,3 +1,9 @@
+//Isaías Acosta Herrera
+//Estructura de datos
+//Taller 03
+//Arbol Expresión
+//4 de octubre de 2024
+//John Corredor
 #ifndef NODOEXPRESION_H
 #define NODOEXPRESION_H
 #include <string.h>
